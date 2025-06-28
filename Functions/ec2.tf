237175b71ea04f97badd9bbc1290d3f3dc3028ac
ravilanka999 <ravi050892@gmail.com>
2025-06-28 15:20:35 +1000

    Terraform

--- Functions/ec2.tf
+++ Functions/ec2.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "roboshop" {
+  count = length(var.instances)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  #vpc_security_group_ids = [ aws_security_group.allow_all.id ]
-  vpc_security_group_ids = local.sg_id
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  #vpc_security_group_ids = local.sg_id
+  tags = merge(
+    var.common_tags,
+    {
+      component = var.instances[count.index]
+      Nmae = var.instances[count.index]
+    }
+
+  )
 }
 
 resource "aws_security_group" "allow_all" {
@@ -27,7 +33,10 @@ resource "aws_security_group" "allow_all" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
+    tags = merge(
+      var.common_tags,
+      {
         Name = "allow-all"
-    }
+      }
+    )
 }
